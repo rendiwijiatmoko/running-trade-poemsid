eaf0136156b6a0450f51aa6c1cff76c2e7e6cf6f
Rendi Wijiatmoko <rendi.os@brins.co.id>
2022-11-17 13:02:45 +0700

    Initial Commit

---
+++ PoemsID/PoemsIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemsIDApp.swift
+//  PoemsID
+//
+//  Created by Rendi Wijiatmoko on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PoemsIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
